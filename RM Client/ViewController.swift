5701119762c0f000076cd22d8bd6bea956defe21
Yazan Tarifi <yazantarifi989@gmail.com>
2023-08-31 01:24:21 +0300

    Generate Project Files

---
+++ RM Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RM Client
+//
+//  Created by Yazan Tarifi on 31/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
